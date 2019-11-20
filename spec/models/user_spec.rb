4b07fb5205f153abb52f3308e8f07992790c6fe0
Hideaki Shirahama <hideaki.shirahama@gmail.com>
2019-11-21 06:47:40 +0900

    add users_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user) }
+  let(:duplicate_user) { user.dup }
 
-  it { is_expected.to validate_presence_of :first_name }
-  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :email }
-  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  # it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
   it 'is valid' do
     expect(user).to be_valid
@@ -32,31 +32,30 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
-  specify 'email validation should accept valid addresses' do
+  specify 'email validation accept valid addresses' do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       user.email = valid_address
-      assert user.valid?, "#{valid_address.inspect} should be valid"
+      expect(user).to be_valid, "#{valid_address.inspect} should be valid"
     end
   end
 
-  # test 'email validation should reject invalid addresses' do
-  #   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-  #                          foo@bar_baz.com foo@bar+baz.com]
-  #   invalid_addresses.each do |invalid_address|
-  #     @user.email = invalid_address
-  #     assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-  #   end
-  # end
+  specify 'email validation reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 
-  # test 'email addresses should be unique' do
-  #   duplicate_user = @user.dup
-  #   @user.save
-  #   assert_not duplicate_user.valid?
-  # end
+  specify 'email addresses is unique' do
+    user.save
+    expect(duplicate_user).to_not be_valid
+  end
 
-  # test 'password should be present (nonblank)' do
+  # specify 'password should be present (nonblank)' do
   #   @user.password = @user.password_confirmation = ' ' * 6
   #   assert_not @user.valid?
   # end
