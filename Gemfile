abed792100eb21e78601deed61ef2ff36cf35b96
Hideaki Shirahama <hideaki.shirahama@gmail.com>
2019-11-25 23:08:05 +0900

    modify TopPage

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 gem 'bcrypt'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'bootstrap-sass'
 gem 'coffee-rails', '~> 4.2'
 gem 'dotenv-rails'
 gem 'jbuilder', '~> 2.5'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,47 @@
 class UsersController < ApplicationController
+  # before_action user_params
+
+  def index
+    # @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    # if @user.update_attributes(params[:user])
+    #   flash[:success] = 'Profile updated'
+    #   sign_in @user
+    #   redirect_to @user
+    # else
+    #   render 'edit'
+    # end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
