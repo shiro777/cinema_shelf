c09b457f9e3c534e044124143d06e7f50732faa9
Hideaki Shirahama <hideaki.shirahama@gmail.com>
2019-12-06 13:38:48 +0900

    fix log_out method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # @user.send_activation_email
+      log_in @user
       flash[:success] = '新規登録が完了しました。'
       redirect_to @user
     else
